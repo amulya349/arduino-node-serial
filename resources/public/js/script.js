3ead2923d6cf3d1d347513990c1e42b32951764c
Amulya Kumar Sahoo <amulya349@gmail.com>
2016-04-16 17:24:06 +0000

    updated frontend

--- resources/public/js/script.js
+++ resources/public/js/script.js
@@ -1,9 +1,9 @@
 var socket = io();
 socket.on('value', function(data){
 	var x = data.split(' ');
-    $('#data-list').append($('<li class="list-group-item">').text('Power: '+x[0]));
-    $('#data-list').append($('<li class="list-group-item">').text('Duration: '+x[1]));
-    $('#data-list').append($('<li class="list-group-item">').text('Cost: '+x[2]));
+    $('#data-list').append($('<li class="list-group-item">').text('Power: '+x[0]+' KW'));
+    $('#data-list').append($('<li class="list-group-item">').text('Duration: '+x[1]+ ' Hours'));
+    $('#data-list').append($('<li class="list-group-item">').text('Cost: '+x[2] +' INR'));
 
     $('#data-list').append($('<li class="list-group-item">').text(''));
   });
